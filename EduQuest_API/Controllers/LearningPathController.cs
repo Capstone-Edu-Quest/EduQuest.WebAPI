758ac165f790f09ea895f17b71b4b7536bd81d5e
Minhduc027 <minhduylongthuan@gmail>
2025-02-01 23:32:42 +0700

    Initial LearningPath base components

---
+++ EduQuest_API/Controllers/LearningPathController.cs
@@ -0,0 +1,50 @@
+﻿using EduQuest_Domain.Constants;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduQuest_API.Controllers;
+[Route(Constants.Http.API_VERSION + "/LearningPath")]
+public class LearningPathController : Controller
+{
+    
+        private ISender _mediator;
+        public LearningPathController(ISender mediator)
+        {
+            _mediator = mediator;
+
+        }
+
+        [HttpGet("me")]
+        public async Task<IActionResult> GetAllUserLearningPath([FromQuery, Range(1, int.MaxValue)] int pageNo = 1, int eachPage = 10, CancellationToken cancellationToken = default)
+        {
+            /*var result = await _mediator.Send(new GetAllUsersQuery(pageNo, eachPage), cancellationToken);
+            return Ok(result);*/
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("detail")]
+        public async Task<IActionResult> GetLearningPathDetail([FromQuery] string learningPathId, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyPublicLearningPath([FromQuery] string learningPathId, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLearningPath([FromBody] string body, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPut]
+        public async Task<IActionResult> UpdateLearningPath([FromBody] string body, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
---
+++ EduQuest_Infrastructure/Repository/LearningPathRepository.cs
@@ -0,0 +1,23 @@
+﻿using EduQuest_Domain.Entities;
+using EduQuest_Domain.Repository;
+using EduQuest_Infrastructure.Persistence;
+using EduQuest_Infrastructure.Repository.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduQuest_Infrastructure.Repository;
+
+public class LearningPathRepository : GenericRepository<LearningPath>, ILearningPathRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public LearningPathRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+
+}
