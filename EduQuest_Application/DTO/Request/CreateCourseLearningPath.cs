776f4306652eec285de575b071a87aaeab57c5cc
Minhduc027 <minhduylongthuan@gmail>
2025-02-01 23:47:40 +0700

    Initial Dto for Leanring Path Feature: CreateDto

---
+++ EduQuest_Application/DTO/Request/CreateCourseLearningPath.cs
@@ -0,0 +1,13 @@
+﻿using EduQuest_Application.Mappings;
+using EduQuest_Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduQuest_Application.DTO.Request;
+
+public class CreateCourseLearningPath: IMapFrom<LearningPathCourse>, IMapTo<LearningPathCourse>
+{
+}
---
+++ EduQuest_Application/DTO/Request/CreateLearningPathRequest.cs
@@ -0,0 +1,15 @@
+﻿using EduQuest_Application.Mappings;
+using EduQuest_Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduQuest_Application.DTO.Request;
+
+public class CreateLearningPathRequest: IMapFrom<LearningPath>, IMapTo<LearningPath>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
