51876fa562b279074113539d8c73e078ca3e9cf3
CongLam1806 <lamtc1806@gmail.com>
2025-02-03 23:38:59 +0700

    update

---
+++ EduQuest_Infrastructure/Migrations/20250203163527_createSystemConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduQuest_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class createSystemConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SystemConfig",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Value = table.Column<double>(type: "float", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SystemConfig", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SystemConfig_DeletedAt",
+                table: "SystemConfig",
+                column: "DeletedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SystemConfig");
+        }
+    }
+}
