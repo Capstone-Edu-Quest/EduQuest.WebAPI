88300dff2ab0af850e1a1bb91c4c7d2aef1f17b8
Minhduc027 <minhduylongthuan@gmail>
2025-03-14 01:19:19 +0700

    Update Coupon feature, update insert data script

--- EduQuest_Domain/Resource/script insert role and package.sql
+++ EduQuest_Domain/Resource/script insert role and package.sql
@@ -1,18 +1,8 @@
--- Insert data into PackagePrivilege table
-INSERT INTO public."PackagePrivilege" ("Id", "Name", "Description", "CreatedAt", "UpdatedBy", "UpdatedAt", "DeletedAt")
-VALUES 
-(1, 'Basic Privilege', 'Access to basic features', NOW(), 'Admin', NOW(), NULL),
-(2, 'Advanced Privilege', 'Access to advanced features', NOW(), 'Admin', NOW(), NULL);
 
--- Insert data into AccountPackage table
-INSERT INTO public."AccountPackage" ("Id", "Name", "Description", "DurationDays", "Price", "IsFree", "CreatedAt", "UpdatedBy", "UpdatedAt", "DeletedAt")
-VALUES 
-(1, 'Free Plan', 'Basic plan with limited access', 30, 0, TRUE, NOW(), 'Admin', NOW(), NULL),
-(2, 'Premium Plan', 'Full access to all features', 365, 100, FALSE, NOW(), 'Admin', NOW(), NULL);
 
 -- Insert data into Role table
 INSERT INTO public."Role" ("Id", "RoleName", "CreatedAt", "UpdatedBy", "UpdatedAt", "DeletedAt")
-VALUES 
+VALUES
 (1, 'Admin', NOW(), 'System', NOW(), NULL),
 (2, 'Instructor', NOW(), 'System', NOW(), NULL),
 (3, 'Learner', NOW(), 'System', NOW(), NULL),
@@ -21,32 +11,38 @@ VALUES
 (6, 'Expert', NOW(), 'System', NOW(), NULL);
 
 INSERT INTO public."SystemConfig" ("Id", "Name", "Value", "Description", "CreatedAt", "UpdatedAt")
-VALUES 
+VALUES
     ('1', 'Video', 1.0, 'Configuration for video', NOW(), NOW()),
     ('2', 'Document', 1.0, 'Configuration for document', NOW(), NOW()),
     ('3', 'Quiz', 0.8, 'Configuration for quiz', NOW(), NOW()),
     ('4', 'Assignment', 1.0, 'Configuration for assignment', NOW(), NOW());
 
 
-INSERT INTO "User" ("Id", "Username", "AvatarUrl", "Email", "Phone", "Headline", "Status", "Description", "RoleId", "PackagePrivilegeId",
-                    "AccountPackageId", "CreatedAt", "UpdatedBy", "UpdatedAt", "DeletedAt")
-VALUES 
-    (1, 'user1', 'avatar1.png', 'user1@example.com', '1234567890', 'Headline 1', 'Active', 'Description 1', 1, 1, NULL, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
-    (2, 'user2', 'avatar2.png', 'user2@example.com', '1234567891', 'Headline 2', 'Active', 'Description 2', 2, 1, NULL, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
-    (3, 'user3', 'avatar3.png', 'user3@example.com', '1234567892', 'Headline 3', 'Active', 'Description 3', 3, 1, NULL, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
-    (4, 'user4', 'avatar4.png', 'user4@example.com', '1234567893', 'Headline 4', 'Active', 'Description 4', 4, 1, NULL, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
-    (5, 'user5', 'avatar5.png', 'user5@example.com', '1234567894', 'Headline 5', 'Active', 'Description 5', 5, 1, NULL, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
-    (6, 'user6', 'avatar6.png', 'user6@example.com', '1234567895', 'Headline 6', 'Active', 'Description 6', 6, 1, NULL, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL);
+INSERT INTO "Subscription" ("Id", "Name", "Description", "DurationDays", "Price", "IsFree", "CreatedAt", "UpdatedAt", "UpdatedBy", "DeletedAt")
+VALUES
+    ('1', 'Package 1', 'des1', 30, 4000, TRUE, CURRENT_TIMESTAMP, NULL, NULL, NULL),
+    ('2', 'Package 2', 'des2', 60, 6000, FALSE, CURRENT_TIMESTAMP, NULL, NULL, NULL),
+    ('3', 'Package 3', 'des3', 90, 8000, FALSE, CURRENT_TIMESTAMP, NULL, NULL, NULL),
+    ('4', 'Package 4', 'des4', 120, 10000, TRUE, CURRENT_TIMESTAMP, NULL, NULL, NULL);
 
-	
-CREATE TABLE public."SystemConfig" (
-    "Id" VARCHAR(50) PRIMARY KEY,  -- Sử dụng VARCHAR thay vì UUID
-    "Name" VARCHAR(255) NOT NULL UNIQUE,
-    "Value" DECIMAL(5,2) NOT NULL,
-    "Description" TEXT,
-    "CreatedAt" TIMESTAMP DEFAULT NOW(),
-    "UpdatedBy" VARCHAR(255),
-    "UpdatedAt" TIMESTAMP,
-    "DeletedAt" TIMESTAMP
-);
+INSERT INTO "User" ("Id", "Username", "AvatarUrl", "Email", "Phone", "Headline", "Status", "Description", "RoleId","SubscriptionId", "CreatedAt", "UpdatedBy", "UpdatedAt", "DeletedAt")
+VALUES
+    (1, 'user1', 'avatar1.png', 'user1@example.com', '1234567890', 'Headline 1', 'Active', 'Description 1', 1, 1, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
+    (2, 'user2', 'avatar2.png', 'user2@example.com', '1234567891', 'Headline 2', 'Active', 'Description 2', 2, 2, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
+    (3, 'user3', 'avatar3.png', 'user3@example.com', '1234567892', 'Headline 3', 'Active', 'Description 3', 3, 3, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
+    (4, 'user4', 'avatar4.png', 'user4@example.com', '1234567893', 'Headline 4', 'Active', 'Description 4', 4, 4, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
+    (5, 'user5', 'avatar5.png', 'user5@example.com', '1234567894', 'Headline 5', 'Active', 'Description 5', 5, 3, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL),
+    (6, 'user6', 'avatar6.png', 'user6@example.com', '1234567895', 'Headline 6', 'Active', 'Description 6', 6, 2, CURRENT_TIMESTAMP, NULL, CURRENT_TIMESTAMP, NULL);
 
+
+
+
+
+INSERT INTO "Course" ("Id", "Title", "Description", "PhotoUrl", "Color", "Price", "Requirement", "Feature", "IsRequired", "CreatedBy",
+                      "CreatedAt", "UpdatedBy", "UpdatedAt", "DeletedAt", "Status")
+VALUES
+    (1, 'Course Title 1', 'Description for Course 1', 'photo1.png', '#FF5733', 99.99, 'Requirement for Course 1', 'Feature for Course 1', TRUE, 2,
+     CURRENT_TIMESTAMP, NULL, null, NULL, 'Published'),
+
+    (2, 'Course Title 2', 'Description for Course 2', 'photo2.png', '#33FF57', 149.99, 'Requirement for Course 2', 'Feature for Course 2', FALSE, 2,
+     CURRENT_TIMESTAMP, NULL, null, NULL, 'Published');
