c244370566eab530949c4cff9ac86fc974228014
CongLam1806 <lamtc1806@gmail.com>
2025-03-31 19:42:31 +0700

    update course detail

--- EduQuest_Application/DTO/Request/Courses/UpdateCourseRequest.cs
+++ EduQuest_Application/DTO/Request/Courses/UpdateCourseRequest.cs
@@ -10,7 +10,7 @@ namespace EduQuest_Application.DTO.Request.Courses
         public string Title { get; set; } = string.Empty;
 		public string? Description { get; set; }
 		public string? PhotoUrl { get; set; }
-		public string? Requirement { get; set; }
+		public List<string>? RequirementList { get; set; }
 		public string? Feature { get; set; }
 		public decimal? Price { get; set; }
 		public bool? IsRequired { get; set; }
