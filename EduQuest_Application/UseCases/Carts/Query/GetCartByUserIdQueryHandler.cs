aae7401d92f93fa740944fea3e7448b727793efe
CongLam1806 <lamtc1806@gmail.com>
2025-04-02 23:54:27 +0700

    update logic add to cart and get cart

--- EduQuest_Application/UseCases/Carts/Query/GetCartByUserIdQueryHandler.cs
+++ EduQuest_Application/UseCases/Carts/Query/GetCartByUserIdQueryHandler.cs
@@ -2,6 +2,7 @@
 using EduQuest_Application.DTO.Response.Carts;
 using EduQuest_Application.DTO.Response.Courses;
 using EduQuest_Application.Helper;
+using EduQuest_Domain.Entities;
 using EduQuest_Domain.Enums;
 using EduQuest_Domain.Models.Response;
 using EduQuest_Domain.Repository;
@@ -40,29 +41,50 @@ namespace EduQuest_Application.UseCases.Carts.Query
 		{
 			var apiResponse = new APIResponse();
 			var cart = await _cartRepository.GetByUserId(request.UserId);
-
 			var cartResponse = _mapper.Map<MyCartReponse>(cart);
-			var listCourseId = cart.CartItems.Select(c => c.CourseId).Distinct().ToList();
-			var listCourse = await _courseRepository.GetByListIds(listCourseId);
-
-			var listCourseResponse = _mapper.Map<List<CourseSearchResponse>>(listCourse);
-			cartResponse.NumOfCourse = listCourse.Count();
-			foreach (var course in listCourseResponse)
+			if (cart != null && cart.CartItems.Any())
 			{
-				var user = await _userRepository.GetById(course.CreatedBy);
-				course.Author = user!.Username!;
+				var listCourseId = cart.CartItems.Select(c => c.CourseId).Distinct().ToList();
+				var listCourse = await _courseRepository.GetByListIds(listCourseId);
 
-				var courseSta = await _courseStatisticRepository.GetByCourseId(course.Id);
-				if (courseSta != null)
+				var listCourseResponse = _mapper.Map<List<CourseSearchResponse>>(listCourse);
+				cartResponse.NumOfCourse = listCourse.Count();
+				foreach (var course in listCourseResponse)
 				{
-					course.TotalLesson = (int)courseSta.TotalLesson;
-					course.TotalReview = (int)courseSta.TotalReview;
-					course.Rating = (int)courseSta.Rating;
-					course.TotalTime = (int)courseSta.TotalTime;
+					var user = await _userRepository.GetById(course.CreatedBy);
+					course.Author = user!.Username!;
+
+					var courseSta = await _courseStatisticRepository.GetByCourseId(course.Id);
+					if (courseSta != null)
+					{
+						course.TotalLesson = (int)courseSta.TotalLesson;
+						course.TotalReview = (int)courseSta.TotalReview;
+						course.Rating = (int)courseSta.Rating;
+						course.TotalTime = (int)courseSta.TotalTime;
+					}
+					course.ProgressPercentage = null;
 				}
-				course.ProgressPercentage = null;
+				cartResponse.Courses = listCourseResponse;
+				return apiResponse = GeneralHelper.CreateSuccessResponse(System.Net.HttpStatusCode.OK, MessageCommon.GetSuccesfully, cartResponse, "name", "cart");
+			} else if(cart == null)
+			{
+				var newCart = new Cart
+				{
+					Id = Guid.NewGuid().ToString(),
+					UserId = request.UserId,
+					Total = 0
+				};
+
+				await _cartRepository.Add(newCart);
+				await _unitOfWork.SaveChangesAsync();
+				cartResponse = _mapper.Map<MyCartReponse>(newCart);
+
+			} else
+			{
+				cartResponse = _mapper.Map<MyCartReponse>(cart);
 			}
-			cartResponse.Courses = listCourseResponse;
+			cartResponse.NumOfCourse = 0;
+			cartResponse.Courses = new List<CourseSearchResponse>();
 			return apiResponse = GeneralHelper.CreateSuccessResponse(System.Net.HttpStatusCode.OK, MessageCommon.GetSuccesfully, cartResponse, "name", "cart");
 
 		}
