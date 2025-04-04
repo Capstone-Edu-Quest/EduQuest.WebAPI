bb8d239bf0c108ccce91ad9adf02dfcc473bdbe7
Minhduc027 <minhduylongthuan@gmail>
2025-04-04 20:35:44 +0700

    clean code

--- EduQuest_Infrastructure/Configurations/AppConfigurationService.cs
+++ EduQuest_Infrastructure/Configurations/AppConfigurationService.cs
@@ -181,7 +181,6 @@ namespace EduQuest_Infrastructure
 			services.AddScoped<ILevelRepository, LevelRepository>();
 			services.AddScoped<IBoosterRepository, BoosterRepository>();
 			services.AddScoped<IStudyTimeRepository, StudyTimeRepository>();
-
 			services.AddScoped<IReportRepository, ReportRepository>();
 
             services.AddSingleton(provider =>
