5bce626d426e352286d9217de5427cbcbcf2e4e7
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2025-04-07 21:04:31 +0700

    new api

---
+++ EduQuest_Application/UseCases/Materials/Command/UploadImage/UploadImageCommand.cs
@@ -0,0 +1,10 @@
+﻿using EduQuest_Domain.Models.Response;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace EduQuest_Application.UseCases.Materials.Command.UploadImage;
+
+public class UploadImageCommand : IRequest<APIResponse>
+{
+    public IFormFile ImageFile { get; set; }
+}
