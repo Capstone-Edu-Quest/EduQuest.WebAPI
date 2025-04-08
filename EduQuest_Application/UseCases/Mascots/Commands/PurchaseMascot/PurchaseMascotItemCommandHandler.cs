a8545e865d904a16aa63b0732959fed2fe37ce45
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2025-04-09 02:34:58 +0700

    minor fix

--- EduQuest_Application/UseCases/Mascots/Commands/PurchaseMascot/PurchaseMascotItemCommandHandler.cs
+++ EduQuest_Application/UseCases/Mascots/Commands/PurchaseMascot/PurchaseMascotItemCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using EduQuest_Application.DTO.Response.Mascot;
+using EduQuest_Application.Helper;
+using EduQuest_Domain.Constants;
 using EduQuest_Domain.Models.Response;
 using EduQuest_Domain.Repository;
 using EduQuest_Domain.Repository.UnitOfWork;
@@ -76,8 +78,12 @@ public class PurchaseMascotItemCommandHandler : IRequestHandler<PurchaseMascotIt
             IsEquipped = false
         };
 
-        //var userdetail = await _userStatisticRepository.GetById(request.UserId);
-        //userdetail.Gold -= (int)shopItem.Price;
+        var userdetail = await _userStatisticRepository.GetById(request.UserId);
+        if (userdetail.Gold < shopItem.Price)
+        {
+            return GeneralHelper.CreateErrorResponse(System.Net.HttpStatusCode.NotFound, Constants.MessageCommon.NotEnoughGold, Constants.MessageCommon.NotEnoughGold, "name", "item");
+        }
+        userdetail.Gold -= (int)shopItem.Price;
 
 
         await _mascotInventoryRepository.Add(mascotInventory);
