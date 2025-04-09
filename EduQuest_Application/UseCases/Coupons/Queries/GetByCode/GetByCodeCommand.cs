b1b74688c209afa199f4b25586bf72b42580766f
Minhduc027 <minhduylongthuan@gmail>
2025-04-09 20:04:45 +0700

    LevelUpdate, New API Get CouponByCode

---
+++ EduQuest_Application/UseCases/Coupons/Queries/GetByCode/GetByCodeCommand.cs
@@ -0,0 +1,9 @@
+﻿using EduQuest_Domain.Models.Response;
+using MediatR;
+
+namespace EduQuest_Application.UseCases.Coupons.Queries.GetByCode;
+
+public class GetByCodeCommand : IRequest<APIResponse>
+{
+    public string Code { get; set; }
+}
