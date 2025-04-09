e39bf39b78f7723fe867d0d1936520c23e7d914e
Minhduc027 <minhduylongthuan@gmail>
2025-04-09 22:17:13 +0700

    fix bug

--- EduQuest_API/Controllers/QuestController.cs
+++ EduQuest_API/Controllers/QuestController.cs
@@ -113,6 +113,10 @@ namespace EduQuest_API.Controllers
         {
             string userId = User.GetUserIdFromToken().ToString();
             var result = await _mediator.Send(new ClaimRewardCommand(userQuestId, userId), token);
+            if(result.Errors != null && result.Errors.StatusResponse == HttpStatusCode.NotFound)
+            {
+                return NotFound(result);
+            }
             return (result.Errors != null && result.Errors.StatusResponse != HttpStatusCode.OK) ? BadRequest(result) : Ok(result);
         }
     }
