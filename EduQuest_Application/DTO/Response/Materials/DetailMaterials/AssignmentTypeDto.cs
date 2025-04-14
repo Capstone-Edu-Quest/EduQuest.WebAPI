59710833f0a661d5273cfd87af9a06fc96dae001
Minhduc027 <minhduylongthuan@gmail>
2025-04-14 23:08:47 +0700

    fix bug no assignment id

--- EduQuest_Application/DTO/Response/Materials/DetailMaterials/AssignmentTypeDto.cs
+++ EduQuest_Application/DTO/Response/Materials/DetailMaterials/AssignmentTypeDto.cs
@@ -5,6 +5,7 @@ namespace EduQuest_Application.DTO.Response.Materials.DetailMaterials;
 
 public class AssignmentTypeDto : IMapFrom<Assignment>, IMapTo<Assignment>
 {
+    public string Id { get; set; }
     public int? TimeLimit { get; set; }
     public string? Question { get; set; }
     public string? AnswerLanguage { get; set; }
