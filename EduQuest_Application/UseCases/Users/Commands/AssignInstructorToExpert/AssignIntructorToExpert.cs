e137bdef1da7752344f104bbb8d14c85e59f154b
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2025-04-18 08:50:49 +0700

    add new apis

---
+++ EduQuest_Application/UseCases/Users/Commands/AssignInstructorToExpert/AssignIntructorToExpert.cs
@@ -0,0 +1,10 @@
+﻿using EduQuest_Domain.Models.Response;
+using MediatR;
+
+namespace EduQuest_Application.UseCases.Users.Commands.AssignInstructorToExpert;
+
+public class AssignIntructorToExpert : IRequest<APIResponse>
+{
+    public string InstructorId { get; set; }
+    public string AssignTo { get; set; }
+}
---
+++ EduQuest_Application/UseCases/Users/Queries/GetUserByAssignToExpert/GetUserByAsignToExpertQuery.cs
@@ -0,0 +1,9 @@
+﻿using EduQuest_Domain.Models.Response;
+using MediatR;
+
+namespace EduQuest_Application.UseCases.Users.Queries.GetUserByAssignToExpert;
+
+public class GetUserByAsignToExpertQuery : IRequest<APIResponse>
+{
+    public string expertId { get; set; }
+}
