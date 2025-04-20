4fdff22a9242fadcbb4ad6cda8afacac29496cbb
Minhduc027 <minhduylongthuan@gmail>
2025-04-21 01:12:47 +0700

    add  leaderboard

---
+++ EduQuest_Application/DTO/Response/Users/UserRankingResponse.cs
@@ -0,0 +1,13 @@
+﻿using EduQuest_Application.Mappings;
+using EduQuest_Domain.Entities;
+
+namespace EduQuest_Application.DTO.Response.Users;
+
+public class UserRankingResponse : IMapFrom<User>
+{
+    public string Id { get; set; } = string.Empty;
+    public string? Username { get; set; }
+    public string? AvatarUrl { get; set; }
+    public double? score { get; set; }
+    public long rank { get; set; }
+}
