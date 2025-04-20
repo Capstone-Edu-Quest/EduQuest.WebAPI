42bb136bd486d5a032edf9a7187661d973c2b049
Minhduc027 <minhduylongthuan@gmail>
2025-04-21 01:50:20 +0700

    fix bug

--- EduQuest_Domain/Repository/IQuizAttemptRepository.cs
+++ EduQuest_Domain/Repository/IQuizAttemptRepository.cs
@@ -1,16 +1,11 @@
 ﻿using EduQuest_Domain.Entities;
 using EduQuest_Domain.Repository.Generic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EduQuest_Domain.Repository;
 
 public interface IQuizAttemptRepository: IGenericRepository<QuizAttempt>
 {
-    Task<int> GetAttemptNo(string quizId, string lessonId);
+    Task<int> GetAttemptNo(string quizId, string lessonId, string userId);
     Task<List<QuizAttempt>> GetQuizAttempts(string quizId, string lessonId, string userId);
     Task<List<QuizAttempt>> GetQuizzesAttempts(List<string> quizId, List<string> lessonId, string userId);
 }
