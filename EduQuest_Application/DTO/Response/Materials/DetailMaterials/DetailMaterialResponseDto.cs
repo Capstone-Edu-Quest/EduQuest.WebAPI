d5e919e458770fb0fc9f7deae8741d5efed4672b
CongLam1806 <lamtc1806@gmail.com>
2025-05-02 21:55:07 +0700

    update material

--- EduQuest_Application/DTO/Response/Materials/DetailMaterials/DetailMaterialResponseDto.cs
+++ EduQuest_Application/DTO/Response/Materials/DetailMaterials/DetailMaterialResponseDto.cs
@@ -3,6 +3,7 @@ using EduQuest_Application.DTO.Request.Materials;
 using EduQuest_Application.DTO.Response.UserStatistics;
 using EduQuest_Application.Mappings;
 using EduQuest_Domain.Entities;
+using EduQuest_Domain.Enums;
 
 namespace EduQuest_Application.DTO.Response.Materials.DetailMaterials;
 
@@ -12,6 +13,7 @@ public class DetailMaterialResponseDto : IMapFrom<Material>, IMapTo<Material>
 	public string? Type { get; set; }
 	public string? Title { get; set; }
 	public string? Description { get; set; }
+	public double? Duration { get; set; }
 	public VideoTypeDto? Video { get; set; }
 	public string? Content { get; set; } //Document
 	public QuizTypeDto? Quiz { get; set; }
@@ -22,8 +24,14 @@ public class DetailMaterialResponseDto : IMapFrom<Material>, IMapTo<Material>
 		profile.CreateMap<Material, DetailMaterialResponseDto>()
 		.ForMember(dest => dest.Quiz, opt => opt.MapFrom(src => src.Quiz))
 		.ForMember(dest => dest.Assignment, opt => opt.MapFrom(src => src.Assignment))
-		.ForPath(dest => dest.Video.UrlMaterial, opt => opt.MapFrom(src => src.UrlMaterial))
-		.ForPath(dest => dest.Video.Duration, opt => opt.MapFrom(src => src.Duration));
-       
+		.ForMember(dest => dest.Video, opt => {
+			opt.PreCondition(src => src.Type == GeneralEnums.TypeOfMaterial.Video.ToString()); // chỉ ánh xạ nếu là video
+			opt.MapFrom(src => new VideoTypeDto
+			{
+				UrlMaterial = src.UrlMaterial,
+			});
+		});
+		//.ForPath(dest => dest.Video.Duration, opt => opt.MapFrom(src => src.Duration));
+
 	}
 }
