a4951e406c6d94f59d68e81e6146293ff9445ccc
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2025-05-04 20:19:32 +0700

    fix bug

--- EduQuest_Application/UseCases/Users/Commands/ApproveBecomeInstructor/ApproveBecomeInstructorCommandHandler.cs
+++ EduQuest_Application/UseCases/Users/Commands/ApproveBecomeInstructor/ApproveBecomeInstructorCommandHandler.cs
@@ -35,6 +35,10 @@ public class ApproveBecomeInstructorCommandHandler : IRequestHandler<ApproveBeco
         ? ((int)GeneralEnums.UserRole.Instructor).ToString()
         : user.RoleId;
         user.Status = request.isApprove ? AccountStatus.Active.ToString() : AccountStatus.Rejected.ToString();
+
+        user.AssignToExpertId = request.isApprove ? user.AssignToExpertId : null;
+        user.RejectedReason = request.isApprove ? null : request.RejectedReason;
+
         await _userRepository.Update(user);
         await _unitOfWork.SaveChangesAsync();
 
@@ -42,8 +46,7 @@ public class ApproveBecomeInstructorCommandHandler : IRequestHandler<ApproveBeco
             ? NotificationMessage.BECOME_INSTRUCTOR_APPROVED
             : NotificationMessage.BECOME_INSTRUCTOR_REJECTED;
 
-        user.AssignToExpertId = request.isApprove ? user.AssignToExpertId : null;
-        user.RejectedReason = request.isApprove ? null : request.RejectedReason;
+      
 
         await _fireBaseRealtimeService.PushNotificationAsync(new NotificationDto
         {
