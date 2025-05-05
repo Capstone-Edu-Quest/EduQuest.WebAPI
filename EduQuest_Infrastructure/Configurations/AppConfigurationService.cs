654b73a29004279c3d131b567893e0e4de86f8ed
Minhduc027 <minhduylongthuan@gmail>
2025-05-05 23:15:44 +0700

    fix bug

--- EduQuest_Infrastructure/Configurations/AppConfigurationService.cs
+++ EduQuest_Infrastructure/Configurations/AppConfigurationService.cs
@@ -230,12 +230,12 @@ namespace EduQuest_Infrastructure
                 var resetQuestsProgress = new JobKey("resetQuestsProgress");
                 var checkJobKey = new JobKey("ProvideCertificates");
 				var LearningPathDueDate = new JobKey("LearningPathDueDate");
-				var Leaderboard = new JobKey("LeaderBoard");
+				//var Leaderboard = new JobKey("LeaderBoard");
 
                 q.AddJob<ResetQuestProgress>(opts => opts.WithIdentity(resetQuestsProgress));
                 q.AddJob<ResetDailyQuest>(opts => opts.WithIdentity(resetDailyQuests));
                 q.AddJob<UpdateLearningPathCourseDueDate>(opts => opts.WithIdentity(checkJobKey));
-				q.AddJob<InitializeLeaderboard>(opt => opt.WithIdentity(Leaderboard));
+				//q.AddJob<InitializeLeaderboard>(opt => opt.WithIdentity(Leaderboard));
 				q.AddJob<UpdateLearningPathCourseDueDate>(opts => opts.WithIdentity(LearningPathDueDate));
                 q.AddTrigger(opts => opts.ForJob(resetDailyQuests)
                 .WithCronSchedule(cronExpression));
@@ -255,8 +255,8 @@ namespace EduQuest_Infrastructure
                         .RepeatForever()
                         .Build()));
 
-				q.AddTrigger(opts => opts.ForJob(Leaderboard)
-				.StartNow());
+				/*q.AddTrigger(opts => opts.ForJob(Leaderboard)
+				.StartNow());*/
 
 				q.AddTrigger(opts => opts
                     .ForJob(LearningPathDueDate)
