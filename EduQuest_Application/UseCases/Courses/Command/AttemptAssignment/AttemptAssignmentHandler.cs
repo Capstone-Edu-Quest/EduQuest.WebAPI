13634601fac1ddb6f5e3c9cc72f61221b616a052
Minhduc027 <minhduylongthuan@gmail>
2025-05-25 21:09:45 +0700

    adjust levelup handler

--- EduQuest_Application/UseCases/Courses/Command/AttemptAssignment/AttemptAssignmentHandler.cs
+++ EduQuest_Application/UseCases/Courses/Command/AttemptAssignment/AttemptAssignmentHandler.cs
@@ -371,7 +371,7 @@ public class AttemptAssignmentHandler : IRequestHandler<AttemptAssignmentCommand
         var meta = user.UserMeta;
         var currentExp = meta.Exp;
         int maxLevel = await _levelRepository.GetMaxLevelNumber();
-        while (currentExp > 250)
+        while (currentExp > 1)
         {
             var currentLevel = await _levelRepository.GetByLevelNum(meta.Level.Value);
             if (currentLevel == null)
